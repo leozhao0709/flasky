1efb14110a01b1c7dfd3eb0b9af65216ede680e2
lzhao <lzhao@shutterfly.com>
2016-04-04 19:04:39 -0700

    add model class

--- hello.py
+++ hello.py
@@ -6,8 +6,10 @@ from flask.ext.moment import Moment
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-
+from flask_sqlalchemy import SQLAlchemy
 import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__, static_folder=os.path.join(os.getcwd(), 'app', 'static'))
 
@@ -19,6 +21,11 @@ app.config['MAIL_PASSWORD'] = os.environ.get('FLASK_MAIL_PASSWORD')
 
 app.config['SECRET_KEY'] = 'hard to guess string'
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://lzhao:@localhost/flasky'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+db = SQLAlchemy(app)
+
 mail = Mail(app)
 moment = Moment(app)
 
@@ -27,42 +34,64 @@ manager = Manager(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	form = NameForm()
-	if form.validate_on_submit():
-		old_name = session['name']
-		if old_name is not None and old_name != form.name.data:
-			flash('Looks like you have changed your name!')
-		session['name'] = form.name.data
-		return redirect(url_for('index'))
-	return render_template('index.html', form=form, name=session['name'])
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 @app.route('/user/<name>')
 def user(name):
-	return render_template('user.html', name=name)
+    return render_template('user.html', name=name)
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-	return render_template('404.html'), 404
+    return render_template('404.html'), 404
 
 
 @app.errorhandler(500)
 def internal_server_error(e):
-	return render_template('500.html'), 500
+    return render_template('500.html'), 500
 
 
 @app.route('/browser')
 def browser():
-	user_agent = request.headers.get('User-Agent')
-	return "<p>Your browser is %s</p>" % user_agent
+    user_agent = request.headers.get('User-Agent')
+    return "<p>Your browser is %s</p>" % user_agent
 
 
 class NameForm(Form):
-	name = StringField('What is your name?', validators=[DataRequired()])
-	submit = SubmitField('Submit')
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 if __name__ == '__main__':
-	# app.run(debug=True)
-	manager.run()
+    # app.run(debug=True)
+    manager.run()
