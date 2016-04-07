038f2db10557f811d612b14ea59937ed85ef3bb5
leozhao0709 <zhao434@usc.edu>
2016-04-06 22:56:07 -0700

    add migration

--- hello.py
+++ hello.py
@@ -8,6 +8,7 @@ from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -25,88 +26,90 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://lzhao:@localhost/flasky'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
+manager = Manager(app)
+
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 mail = Mail(app)
 moment = Moment(app)
 
-manager = Manager(app)
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            session['known'] = False
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        # old_name = session.get('name')
-        # if old_name is not None and old_name != form.name.data:
-        #     flash('Looks like you have changed your name!')
-        # session['name'] = form.name.data
-        return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
+	form = NameForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(username=form.name.data).first()
+		if user is None:
+			user = User(username=form.name.data)
+			db.session.add(user)
+			session['known'] = False
+		else:
+			session['known'] = True
+		session['name'] = form.name.data
+		# old_name = session.get('name')
+		# if old_name is not None and old_name != form.name.data:
+		#     flash('Looks like you have changed your name!')
+		# session['name'] = form.name.data
+		return redirect(url_for('index'))
+	return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
 
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name)
+	return render_template('user.html', name=name)
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+	return render_template('404.html'), 404
 
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'), 500
+	return render_template('500.html'), 500
 
 
 @app.route('/browser')
 def browser():
-    user_agent = request.headers.get('User-Agent')
-    return "<p>Your browser is %s</p>" % user_agent
+	user_agent = request.headers.get('User-Agent')
+	return "<p>Your browser is %s</p>" % user_agent
 
 
 class NameForm(Form):
-    name = StringField('What is your name?', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+	name = StringField('What is your name?', validators=[DataRequired()])
+	submit = SubmitField('Submit')
 
 
 class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
+	__tablename__ = 'roles'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64), unique=True)
 
-    users = db.relationship('User', backref='role', lazy='dynamic')
+	users = db.relationship('User', backref='role', lazy='dynamic')
 
-    def __repr__(self):
-        return '<Role %r>' % self.name
+	def __repr__(self):
+		return '<Role %r>' % self.name
 
 
 class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
+	__tablename__ = 'users'
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(64), unique=True, index=True)
 
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+	def __repr__(self):
+		return '<User %r>' % self.username
 
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role)
+	return dict(app=app, db=db, User=User, Role=Role)
+
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 
-
 if __name__ == '__main__':
-    # app.run(debug=True)
-    manager.run()
+	# app.run(debug=True)
+	manager.run()
