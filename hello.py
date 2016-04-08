4966f28bb4663cf87e69af6777b73a3fc284c903
lzhao <lzhao@shutterfly.com>
2016-04-07 18:24:26 -0700

    add mail part

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask import request
-from flask.ext.mail import Mail
+from flask.ext.mail import Mail, Message
 from flask_script import Manager
 from flask_script import Shell
 from flask.ext.moment import Moment
@@ -9,6 +9,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
+from threading import Thread
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -20,6 +21,9 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('FLASK_MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('FLASK_MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <flasky@eample.com>'
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASK_MAIL_ADMIN')
 
 app.config['SECRET_KEY'] = 'hard to guess string'
 
@@ -36,80 +40,94 @@ mail = Mail(app)
 moment = Moment(app)
 
 
+def send_async_email(app, msg):
+    with app.app_context():
+        print "in another thread"
+        mail.send(msg)
+
+
+def send_mail(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.run()
+    return thr
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	form = NameForm()
-	if form.validate_on_submit():
-		user = User.query.filter_by(username=form.name.data).first()
-		if user is None:
-			user = User(username=form.name.data)
-			db.session.add(user)
-			session['known'] = False
-		else:
-			session['known'] = True
-		session['name'] = form.name.data
-		# old_name = session.get('name')
-		# if old_name is not None and old_name != form.name.data:
-		#     flash('Looks like you have changed your name!')
-		# session['name'] = form.name.data
-		return redirect(url_for('index'))
-	return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_mail(app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
 
 
 @app.route('/user/<name>')
 def user(name):
-	return render_template('user.html', name=name)
+    return render_template('user.html', name=name)
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-	return render_template('404.html'), 404
+    return render_template('404.html'), 404
 
 
 @app.errorhandler(500)
 def internal_server_error(e):
-	return render_template('500.html'), 500
+    return render_template('500.html'), 500
 
 
 @app.route('/browser')
 def browser():
-	user_agent = request.headers.get('User-Agent')
-	return "<p>Your browser is %s</p>" % user_agent
+    user_agent = request.headers.get('User-Agent')
+    return "<p>Your browser is %s</p>" % user_agent
 
 
 class NameForm(Form):
-	name = StringField('What is your name?', validators=[DataRequired()])
-	submit = SubmitField('Submit')
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
 class Role(db.Model):
-	__tablename__ = 'roles'
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(64), unique=True)
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
 
-	users = db.relationship('User', backref='role', lazy='dynamic')
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
-	def __repr__(self):
-		return '<Role %r>' % self.name
+    def __repr__(self):
+        return '<Role %r>' % self.name
 
 
 class User(db.Model):
-	__tablename__ = 'users'
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(64), unique=True, index=True)
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
 
-	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
-	def __repr__(self):
-		return '<User %r>' % self.username
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 def make_shell_context():
-	return dict(app=app, db=db, User=User, Role=Role)
+    return dict(app=app, db=db, User=User, Role=Role)
 
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 
 if __name__ == '__main__':
-	# app.run(debug=True)
-	manager.run()
+    # app.run(debug=True)
+    manager.run()
