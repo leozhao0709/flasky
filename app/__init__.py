7dcd7d86048d9b9020293c0e6b96dad6681ae076
lzhao <lzhao@shutterfly.com>
2016-04-11 19:19:49 -0700

    fix session bug

--- app/__init__.py
+++ app/__init__.py
@@ -24,7 +24,7 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
-    # app.permanent_session_lifetime = timedelta(days=7)
+    app.permanent_session_lifetime = timedelta(days=7)
 
     mail.init_app(app)
     moment.init_app(app)
--- app/auth/views.py
+++ app/auth/views.py
@@ -16,8 +16,8 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
-            # if form.remember_me.data:
-            #     session.permanent = True
+            if form.remember_me.data:
+                session.permanent = True
             login_user(user, form.remember_me.data)
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid username or password')
