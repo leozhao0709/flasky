f20cdd96cc41de000e889f709c2ce9845c304d68
leozhao0709 <zhao434@usc.edu>
2016-04-15 00:36:25 -0700

    reset password

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -12,26 +12,38 @@ from app.models import User
 
 
 class LoginForm(Form):
-    email = EmailField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(4, 64)])
-    remember_me = BooleanField('keep me logged in')
-    submit = SubmitField('Log In')
+	email = EmailField('Email', validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired(), Length(4, 64)])
+	remember_me = BooleanField('keep me logged in')
+	submit = SubmitField('Log In')
 
 
 class RegistrationForm(Form):
-    email = EmailField('Email', validators=[DataRequired(), Email()])
+	email = EmailField('Email', validators=[DataRequired(), Email()])
 
-    username = StringField('Username', validators=[DataRequired(), Length(3, 64), Regexp('^[A-Za-z][\w\d_.]*$', 0,
-                                                                                         'username must start with letter and can only contain letter, number, dot or under_score')])
-    password = PasswordField('Password', validators=[DataRequired(), Length(4, 64)])
-    password2 = PasswordField('Password2', validators=[DataRequired(), Length(4, 64),
-                                                       EqualTo('password', message='Password must match')])
-    submit = SubmitField('Register')
+	username = StringField('Username', validators=[DataRequired(), Length(3, 64), Regexp('^[A-Za-z][\w\d_.]*$', 0,
+																						 'username must start with letter and can only contain letter, number, dot or under_score')])
+	password = PasswordField('Password', validators=[DataRequired(), Length(4, 64)])
+	password2 = PasswordField('Password2', validators=[DataRequired(), Length(4, 64),
+													   EqualTo('password', message='Password must match')])
+	submit = SubmitField('Register')
 
-    def validate_email(self, field):
-        if User.query.filter_by(email=field.data).first():
-            raise ValidationError('Email already registered.')
+	def validate_email(self, field):
+		if User.query.filter_by(email=field.data).first():
+			raise ValidationError('Email already registered.')
 
-    def validate_username(self, field):
-        if User.query.filter_by(username=field.data).first():
-            raise ValidationError('Username already in use.')
+	def validate_username(self, field):
+		if User.query.filter_by(username=field.data).first():
+			raise ValidationError('Username already in use.')
+
+
+class PasswordResetRequestForm(Form):
+	email = EmailField('Email', validators=[DataRequired(), Email()])
+	submit = SubmitField('Reset Password')
+
+
+class PasswordResetForm(Form):
+	password = PasswordField('New Password', validators=[DataRequired()])
+	password2 = PasswordField('Confirm password',
+							  validators=[DataRequired(), EqualTo('password', message='Password must match')])
+	submit = SubmitField('Reset Password')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,89 +1,144 @@
 # -*- coding: utf-8 -*-
-from flask import render_template, redirect, request, url_for, flash, session
-from . import auth
-from flask.ext.login import login_user, login_required, logout_user, current_user
-from app.models import User
-from .forms import LoginForm, RegistrationForm
-from app import db
-from app.email import send_email
 
 __author__ = 'lzhao'
 __date__ = '4/10/16'
 __time__ = '6:33 PM'
 
+from flask import render_template, redirect, request, url_for, flash, session, current_app
+from . import auth
+from flask.ext.login import login_user, login_required, logout_user, current_user
+from app.models import User
+from .forms import LoginForm, RegistrationForm, PasswordResetRequestForm, PasswordResetForm
+from app import db
+from app.email import send_email
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user is not None and user.verify_password(form.password.data):
-            if form.remember_me.data:
-                session.permanent = True
-            login_user(user, form.remember_me.data)
-            return redirect(request.args.get('next') or url_for('main.index'))
-        flash('Invalid username or password')
-    elif form.errors.items():
-        for field, errors in form.errors.items():
-            flash(field + ": " + errors[0])
-    return render_template('auth/login.html', form=form)
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(email=form.email.data).first()
+		if user is not None and user.verify_password(form.password.data):
+			if form.remember_me.data:
+				session.permanent = True
+			login_user(user, form.remember_me.data)
+			return redirect(request.args.get('next') or url_for('main.index'))
+		flash('Invalid username or password', 'flashMessage_error')
+	elif form.errors.items():
+		for field, errors in form.errors.items():
+			flash(field + ": " + errors[0], 'flashMessage_error')
+	return render_template('auth/login.html', form=form)
 
 
 @auth.route('/logout')
 @login_required
 def logout():
-    logout_user()
-    flash('You have been logged out')
-    return redirect(url_for('main.index'))
+	logout_user()
+	flash('You have been logged out', 'flashMessage')
+	return redirect(url_for('main.index'))
 
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(email=form.email.data, username=form.username.data, password=form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        token = user.generate_confirmation_token()
-        send_email(user.email, 'Confirm your account', 'auth/email/confirm', user=user, token=token)
-        flash('A confirmation email has been sent to you by email')
-        return redirect(url_for('main.index'))
-    elif form.errors.items():
-        for field, errors in form.errors.items():
-            flash(field + ": " + errors[0])
-    return render_template('auth/register.html', form=form)
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+		db.session.add(user)
+		db.session.commit()
+		token = user.generate_confirmation_token()
+		send_email(user.email, 'Confirm your account', 'auth/email/confirm', user=user, token=token)
+		flash('A confirmation email has been sent to you by email', 'flashMessage')
+		return redirect(url_for('main.index'))
+	elif form.errors.items():
+		for field, errors in form.errors.items():
+			flash(field + ": " + errors[0], 'flashMessage_error')
+	return render_template('auth/register.html', form=form)
 
 
 @auth.route("/confirm/<token>")
 @login_required
 def confirm(token):
-    if current_user.confirmed:
-        return redirect(url_for('main.index'))
-    if current_user.confirm(token):
-        flash('You have confirmed your accounts. Thanks!')
-    else:
-        flash('The confirmation link is invalid or has expired.')
-    return redirect(url_for('main.index'))
+	if current_user.confirmed:
+		return redirect(url_for('main.index'))
+	if current_user.confirm(token):
+		flash('You have confirmed your accounts. Thanks!', 'flashMessage')
+	else:
+		flash('The confirmation link is invalid or has expired.', 'flashMessage_error')
+	return redirect(url_for('main.index'))
 
 
 @auth.before_app_request
 def before_request():
-    if current_user.is_authenticated and not current_user.confirmed and request.endpoint[:5] != 'auth.':
-        return redirect(url_for('auth.unconfirmed'))
+	if current_user.is_authenticated and not current_user.confirmed and request.endpoint[:5] != 'auth.':
+		return redirect(url_for('auth.unconfirmed'))
 
 
 @auth.route('/unconfirmed')
 def unconfirmed():
-    if current_user.is_anonymous or current_user.confirmed:
-        return redirect(url_for('main.index'))
-    return render_template('auth/unconfirmed.html')
+	if current_user.is_anonymous or current_user.confirmed:
+		return redirect(url_for('main.index'))
+	return render_template('auth/unconfirmed.html')
 
 
 @auth.route('/confirm')
 @login_required
 def resend_confirmation():
-    token = current_user.generate_confirmation_token()
-    send_email(current_user.email, 'Confirm Your Account',
-               'auth/email/confirm', user=current_user, token=token)
-    flash('A new confirmation email has been sent to you by email.')
-    return redirect(url_for('main.index'))
+	token = current_user.generate_confirmation_token()
+	send_email(current_user.email, 'Confirm Your Account',
+			   'auth/email/confirm', user=current_user, token=token)
+	flash('A new confirmation email has been sent to you by email.', 'flashMessage')
+	return redirect(url_for('main.index'))
+
+
+@auth.route("/resetpasswordrequest", methods=['GET', 'POST'])
+def password_reset_request():
+	if not current_user.is_anonymous:
+		return redirect(url_for('main.index'))
+	form = PasswordResetRequestForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(email=form.email.data).first()
+		if user:
+			token = user.generate_reset_token()
+			send_email(user.email, 'Reset Your Password', 'auth/email/reset_password', user=user, token=token,
+					   next=request.args.get('next'))
+			flash('An email with instructions to reset your password has been sent to you.', 'flashMessage')
+			return redirect(url_for('auth.login'))
+		else:
+			flash("The user email doesn't exist! ", 'flashMessage_error')
+			return redirect(url_for('auth.password_reset_request'))
+	elif form.errors.items():
+		for field, errors in form.errors.items():
+			flash(field + ": " + errors[0], 'flashMessage_error')
+	return render_template('auth/reset_password_request.html', form=form)
+
+
+@auth.route('/reset/<token>', methods=['GET', 'POST'])
+def password_reset(token):
+	if not current_user.is_anonymous:
+		return redirect(url_for('main.index'))
+
+	s = Serializer(current_app.config['SECRET_KEY'])
+	try:
+		data = s.loads(token)
+	except:
+		flash('The reset link is invalid or has expired.', 'flashMessage_error')
+		return redirect(url_for('main.index'))
+	user_id = data.get('reset')
+	user = User.query.filter_by(id=user_id).first()
+	if user is None:
+		flash('The reset link is invalid or has expired.', 'flashMessage_error')
+		return redirect(url_for('main.index'))
+
+	form = PasswordResetForm()
+	if form.validate_on_submit():
+		if user.reset_password(token, form.password.data):
+			flash('Your password has been updated.', 'flashMessage')
+			return redirect(url_for('auth.login'))
+		else:
+			flash('The reset link is invalid or has expired.', 'flashMessage_error')
+			return redirect(url_for('main.index'))
+	elif form.errors.items():
+		for field, errors in form.errors.items():
+			flash(field + ": " + errors[0], 'flashMessage_error')
+	return render_template('auth/reset_password.html', form=form, email=user.email)
