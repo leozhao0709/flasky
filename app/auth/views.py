7f47204d71973bb8529cdeb3f32b57829e900e86
lzhao <lzhao@shutterfly.com>
2016-04-15 19:27:08 -0700

    change flash category

--- app/auth/views.py
+++ app/auth/views.py
@@ -35,7 +35,7 @@ def login():
 @login_required
 def logout():
 	logout_user()
-	flash('You have been logged out', 'flashMessage')
+	flash('You have been logged out')
 	return redirect(url_for('main.index'))
 
 
@@ -48,7 +48,7 @@ def register():
 		db.session.commit()
 		token = user.generate_confirmation_token()
 		send_email(user.email, 'Confirm your account', 'auth/email/confirm', user=user, token=token)
-		flash('A confirmation email has been sent to you by email', 'flashMessage')
+		flash('A confirmation email has been sent to you by email')
 		return redirect(url_for('main.index'))
 	elif form.errors.items():
 		for field, errors in form.errors.items():
@@ -62,7 +62,7 @@ def confirm(token):
 	if current_user.confirmed:
 		return redirect(url_for('main.index'))
 	if current_user.confirm(token):
-		flash('You have confirmed your accounts. Thanks!', 'flashMessage')
+		flash('You have confirmed your accounts. Thanks!')
 	else:
 		flash('The confirmation link is invalid or has expired.', 'flashMessage_error')
 	return redirect(url_for('main.index'))
@@ -87,7 +87,7 @@ def resend_confirmation():
 	token = current_user.generate_confirmation_token()
 	send_email(current_user.email, 'Confirm Your Account',
 			   'auth/email/confirm', user=current_user, token=token)
-	flash('A new confirmation email has been sent to you by email.', 'flashMessage')
+	flash('A new confirmation email has been sent to you by email.')
 	return redirect(url_for('main.index'))
 
 
@@ -102,7 +102,7 @@ def password_reset_request():
 			token = user.generate_reset_token()
 			send_email(user.email, 'Reset Your Password', 'auth/email/reset_password', user=user, token=token,
 					   next=request.args.get('next'))
-			flash('An email with instructions to reset your password has been sent to you.', 'flashMessage')
+			flash('An email with instructions to reset your password has been sent to you.')
 			return redirect(url_for('auth.login'))
 		else:
 			flash("The user email doesn't exist! ", 'flashMessage_error')
@@ -133,7 +133,7 @@ def password_reset(token):
 	form = PasswordResetForm()
 	if form.validate_on_submit():
 		if user.reset_password(token, form.password.data):
-			flash('Your password has been updated.', 'flashMessage')
+			flash('Your password has been updated.')
 			return redirect(url_for('auth.login'))
 		else:
 			flash('The reset link is invalid or has expired.', 'flashMessage_error')
