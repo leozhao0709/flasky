d896d55c539343d09969b70eac018986ad6c11e6
lzhao <lzhao@shutterfly.com>
2016-04-15 19:22:30 -0700

    add some role stuff

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config:
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     FLASK_MAIL_SUBJECT_PREFIX = '[Flask]'
     FLASK_MAIL_SENDER = 'Flask Admin <flask@example.com>'
-    FLASK_ADMIN = os.environ.get('FLASK_ADMIN')
+    FLASK_ADMIN = os.environ.get('FLASK_MAIL_USERNAME')
 
     @staticmethod
     def init_app(app):
