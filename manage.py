a3edd1205dd2623c29ed47a6bb544c79f88488b6
leozhao0709 <zhao434@usc.edu>
2016-04-18 23:36:23 -0700

    fake data

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 
 import os
 from app import create_app, db
-from app.models import User, Role
+from app.models import User, Role, Post
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
@@ -16,7 +16,7 @@ Migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role)
+    return dict(app=app, db=db, User=User, Role=Role, Post=Post)
 
 
 @manager.command
