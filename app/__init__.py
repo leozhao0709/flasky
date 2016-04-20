6162acb8388fd8f16355dcff8ba58a65bc9586a4
lzhao <lzhao@shutterfly.com>
2016-04-19 19:09:14 -0700

    add pagedown

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from flask import Flask
 from flask.ext.mail import Mail
 from flask.ext.moment import Moment
 from flask.ext.login import LoginManager
+from flask_pagedown import PageDown
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 from datetime import timedelta
@@ -15,6 +16,7 @@ from datetime import timedelta
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+pagedown = PageDown()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
@@ -30,6 +32,7 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    pagedown.init_app(app)
 
     from main import main as main_blueprint
     from auth import auth as auth_blueprint
--- app/main/forms.py
+++ app/main/forms.py
@@ -9,6 +9,7 @@ __time__ = '9:10 PM'
 from flask_wtf import Form
 from wtforms import StringField, SubmitField, TextAreaField, BooleanField, SelectField
 from wtforms.validators import DataRequired, Length, Email, Regexp, ValidationError
+from flask_pagedown.fields import PageDownField
 
 
 class EditProfileForm(Form):
@@ -44,5 +45,5 @@ class EditProfileAdminForm(Form):
 
 
 class PostForm(Form):
-	body = TextAreaField("What's on your mind?", validators=[DataRequired()])
+	body = PageDownField("What's on your mind?", validators=[DataRequired()])
 	submit = SubmitField('Submit')
