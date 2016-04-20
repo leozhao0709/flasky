0e1f471d9a71f7287ee657051b72bdad43aa77eb
leozhao0709 <zhao434@usc.edu>
2016-04-19 23:59:06 -0700

    html

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
+from markdown import markdown
+import bleach
 
 __author__ = 'lzhao'
 __date__ = '3/27/16'
@@ -171,6 +173,7 @@ class Post(db.Model):
 	body = db.Column(db.TEXT)
 	timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 	author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+	body_html = db.Column(db.TEXT)
 
 	@staticmethod
 	def generate_fake(count=100):
@@ -180,12 +183,23 @@ class Post(db.Model):
 		seed()
 		user_count = User.query.count()
 		for i in xrange(count):
-			u = User.query.offset(randint(0, user_count-1)).first()
+			u = User.query.offset(randint(0, user_count - 1)).first()
 			p = Post(body=forgery_py.lorem_ipsum.sentences(randint(1, 3)),
 					 timestamp=forgery_py.date.date(True),
 					 author=u)
 			db.session.add(p)
 			db.session.commit()
+
+	@staticmethod
+	def on_change_body(target, value, oldvalue, initiator):
+		allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+						'h1', 'h2', 'h3', 'p']
+		target.body_html = bleach.linkify(bleach.clean(
+			markdown(value, output_format='html'),
+			tags=allowed_tags, strip=True
+		))
+
+db.event.listen(Post.body, 'set', Post.on_change_body)
 
 
 login_manager.anonymous_user = AnonymousUser
